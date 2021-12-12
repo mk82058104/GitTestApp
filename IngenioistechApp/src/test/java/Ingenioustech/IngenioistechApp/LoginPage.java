7881c923940f31032a8ab808df035efd87891be3
mohit <mk82058104@gmail.com>
2021-12-12 12:00:08 +0530

    This is first commit

---
+++ IngenioistechApp/src/test/java/Ingenioustech/IngenioistechApp/LoginPage.java
@@ -0,0 +1,12 @@
+package Ingenioustech.IngenioistechApp;
+
+import org.testng.annotations.Test;
+
+public class LoginPage {
+	
+	@Test
+	public void tc_01(){
+		System.out.println("**** tc_01 ****");
+	}
+
+}
